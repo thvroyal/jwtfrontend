9d060c7f0950d307f87105c916cf10de07d859f5
Hoang Viet <thvroyal@gmail.com>
2021-04-15 17:06:41 +0700

    finish

---
+++ src/components/LineChar.vue
@@ -0,0 +1,73 @@
+<script>
+import { Line } from "vue-chartjs";
+const options = {
+  responsive: false,
+  plugins: {
+    legend: {
+      position: "bottom",
+    },
+    title: {
+      display: true,
+      text: "Chart Demo",
+    },
+  },
+  scales: {
+    y: {
+      min: 0,
+      title: "Time",
+      max: 110,
+      ticks: {
+        stepSize: 10,
+      },
+    },
+  },
+};
+
+export default {
+  name: "LineChart",
+  extends: Line,
+  data() {
+    return {
+      dataCollection: [100, 8, 23, 4, 565, 3, 25, 45, 2343, 9],
+      data: {
+        labels: this.labelSize(0, 30, 1),
+        datasets: [
+          {
+            label: "Dataset 1",
+            data: this.randomData(30, 100),
+            borderColor: "#007bff",
+            backgroundColor: "transparent",
+          },
+          {
+            label: "Dataset 2",
+            data: this.randomData(30, 100),
+            borderColor: "#dc3545",
+            backgroundColor: "transparent",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    randomData(num, max) {
+      let arrData = [];
+      for (let i = 0; i < num; i++) {
+        arrData.push(Math.floor(Math.random() * max));
+      }
+      return arrData;
+    },
+    labelSize(min, max, step) {
+      let arrlabel = [];
+      for (let i = min; i <= max; i += step) {
+        arrlabel.push(i);
+      }
+      return arrlabel;
+    },
+  },
+  mounted() {
+    this.renderChart(this.data, options);
+  },
+};
+</script>
+
+<style></style>
--- src/views/admin/screenA.vue
+++ src/views/admin/screenA.vue
@@ -1,8 +1,14 @@
 <template>
-  <h1>Hello Admin</h1>
+  <div class="row mt-5 ml-5">
+    <div class="col-8 position-relative">
+      <LineChart />
+    </div>
+  </div>
 </template>
 <script>
+import LineChart from "../../components/LineChar.vue";
 export default {
-  name: "ScreenA",
+  name: "Screen A",
+  components: { LineChart },
 };
 </script>
